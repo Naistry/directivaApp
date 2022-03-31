6b2e2aa6a86f1642c4b73c38ab154e40621e4c25
Martin Pastorino <martinpastorino013@gmail.com>
2022-03-31 15:33:27 -0300

    ngElse, ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,24 @@ export class AppComponent {
   apellido:string = "";
   mensaje:string = "";
   registrado = false;
+  entradas: any;
+
+  constructor(){
+    this.entradas= [
+
+      {titulo: 'Python cada día más presente'},
+      {titulo: 'Java presente hace más de 20 años'},
+      {titulo: 'JavaScript cada vez más funcional'},
+      {titulo: 'Kotlin potencia para tus apps'},
+      {titulo: '¿Dónde quedó pascal?'},
+    ]
+  }
  
   registrarUsario(){
     this.registrado= true;
     this.mensaje= "Usario registrado con éxtio con el nombre de: "+ this.nombre +" "+ this.apellido;
     
   }
+
+  
 }
